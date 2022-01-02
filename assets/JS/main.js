519cc35bc135beb7a7f5826cf42865add28c676d
Cesar Gonzalez <cesargonzalezdc@gmail.com>
2022-01-02 16:37:15 -0300

    Estuctura HTML sin errores, identada y documentada. Css ordenado y depurado. JavaScript ordenado. Elementos modal y tooltips utilizados. Faltando Formulario y revision final

---
+++ assets/JS/main.js
@@ -0,0 +1,15 @@
+$(function(){ //jquery general
+//Modal Imagenes Destacados
+    $("p").dblclick(function(){
+       $(this).hide();
+    });
+
+     $('[data-ds-toggler ="tooltip"]').tooltip();
+
+
+
+
+
+
+
+});   
