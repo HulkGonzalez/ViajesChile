0d97f6e2645afab4b49a9aac282cd3f2c33bf934
Cesar Gonzalez <cesargonzalezdc@gmail.com>
2022-01-02 22:30:43 -0300

    Se corrige funcion en las Card, se agrega una alerta en el tootips y se adjunta el Formulario

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,15 +1,18 @@
 $(function(){ //jquery general
-//Modal Imagenes Destacados
-    $("p").dblclick(function(){
-       $(this).hide();
-    });
-
-     $('[data-ds-toggler ="tooltip"]').tooltip();
-
-
-
-
-
+//
+    //$('.receta-texto').dblclick(function(){
+      //  $(this).hide();
+    //});
+//Tooltip
+    $('[data-ds-toggler ="tooltip"]').tooltip();
+//Alerta
+    $('#boton').click(function(){
+        alert("Codigo Ganador WRT83SU");
+    })
+//Toggle Texto Destacado
+    $('.card-title').click(function(){
+        $('.card-text').toggle();
+    })
 
 
 });   
